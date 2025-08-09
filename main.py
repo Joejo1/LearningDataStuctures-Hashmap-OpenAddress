f94d3ae165c37996954456b6445ac98e29fcf419
Joejo1 <thetester259@gmail.com>
2025-08-09 12:46:17 -0400

    First draft of hashmap

---
+++ main.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+import csv
+
+emtyKey = '........'
+ar = np.array([[emtyKey]* 2] * 500000)
+ln = len(ar)
+numOfCollision = 0
+
+#------------------methods
+
+def getHash(key, arLen):
+    index = hash(key) % arLen
+    return index
+
+def setArKey(index, key):
+    ar[index][0] = key
+
+def isKeyMatching(index, key):
+    if ar[index][0] == key:
+        return True
+    else:
+        return False
+
+def isCollision(index, key):
+    if ar[index][0] == emtyKey:
+        return False
+    else:
+        return True
+
+#--------------------MAIN
+
+js = np.array(pd.read_csv("baby-names.csv"))
+
+lengthOfCSV = len(js)
+print(lengthOfCSV)
+j = 0
+
+for i in js:
+    key = js[j][1]
+
+    index = getHash(key, ln)
+
+    collision = False
+    collision = isCollision(index, key)
+    collisionOffset = 0
+    while collision == True:
+        collisionOffset = collisionOffset+1
+        collision = isCollision(index-collisionOffset, key)
+        numOfCollision = numOfCollision + 1
+
+    ar[index-collisionOffset][0] = key
+    collisionOffset = 0
+
+    j = j + 1
+
+
+
+
+#----------Check how many are populated
+emty = 0
+full = 0
+l = 0
+for k in ar:
+    if ar[l][0] == emtyKey:
+        emty = emty + 1
+    else:
+        full = full + 1
+
+    l = l + 1
+
+
+with open("output.csv","w+") as my_csv:
+    csvWriter = csv.writer(my_csv,delimiter=',')
+    csvWriter.writerows(ar)
+
+#----------Printing-----------
+print(full, emty)
+
+print('number of collisions: ', numOfCollision)
+
+print(js)
+
+print(ar)
+
